99fbe8045afddffcf290a938e7c17a409f2d5732
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2026-06-03 18:48:08 +0800

    fix runner

--- src/textual_drivers/demo/__main__.py
+++ src/textual_drivers/demo/__main__.py
@@ -3,15 +3,27 @@ import argparse
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Textual Drivers Demo")
-    parser.add_argument("--demo", choices=["check_image_support", "try_both"], default="check_image_support", help="Choose a demo to run")
+    parser.add_argument(
+        "--demo",
+        choices=["check_image_support", "try_both", "kitty_drag_out"],
+        default="check_image_support",
+        help="Choose a demo to run",
+    )
     args = parser.parse_args()
 
     if args.demo == "check_image_support":
-        from .capability_check_app import CapabilityCheckApp, _Driver
-        CapabilityCheckApp(driver_class=_Driver).run()
+        from .capability_check_app import CapabilityCheckApp
+
+        CapabilityCheckApp().run()
     elif args.demo == "try_both":
-        from .test_app import DriverTestApp, _Driver
-        DriverTestApp(driver_class=_Driver).run()
+        from .test_app import DriverTestApp
+
+        DriverTestApp().run()
+    elif args.demo == "kitty_drag_out":
+        from .drag_out import DragOutApp
+
+        DragOutApp().run()
+
 
 if __name__ == "__main__":
     main()
